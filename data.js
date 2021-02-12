a47f1daee44ee9c6c8da613babc75fad58682889
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2021-02-11 21:51:37 -0600

    updated data.js

--- data.js
+++ data.js
@@ -1,4 +1,8 @@
-export default[
-    {hotspot1:"You have choosen Area 1"},
-    {hotspot2:"You have choosen Area 2"}
-]
+export default [
+    {
+        hotspot1 : "Clicked on Green color"
+    },
+    {
+        hotspot2 : "Clicked on Gray color"
+    }
+];
